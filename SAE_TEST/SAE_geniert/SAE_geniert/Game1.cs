c02d1ba58ad72a60f0e8e630017c9a10c2a66580
Valentin Falquet <Valentin.Falquet@etu.univ-smb.fr>
2023-01-04 18:02:18 +0100

    perso deplacements pas fini et essai map Alexandre

--- SAE_TEST/SAE_geniert/SAE_geniert/Game1.cs
+++ SAE_TEST/SAE_geniert/SAE_geniert/Game1.cs
@@ -60,8 +60,9 @@ namespace SAE_geniert
             _tiledMap = Content.Load<TiledMap>("mapGenerale");
             _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
             // TODO: use this.Content to load your game content here
-            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("VikiAnimations.sf", new JsonContentLoader());
+            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("BryaAnimations.sf", new JsonContentLoader());
             _perso = new AnimatedSprite(spriteSheet);
+            mapLayer = _tiledMap.GetLayer<TiledMapTileLayer>("obstacles");
         }
 
         protected override void Update(GameTime gameTime)
@@ -71,15 +72,96 @@ namespace SAE_geniert
             _tiledMapRenderer.Update(gameTime);
             // TODO: Add your update logic here
 
-            
+            float deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds; // DeltaTime
+            float walkSpeed = deltaSeconds * _vitessePerso; // Vitesse de déplacement du sprite
+            KeyboardState keyboardState = Keyboard.GetState();
+            String animation = "idle";
+            _perso.Update(deltaSeconds); // time écoulé
+
+            _keyboardState = Keyboard.GetState();
+
+
+            //-=-=-=-=-=-=-=-=-=-DROITE-=-=-=-=-=-=-=-=-=-\\
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth + 1);
+                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileHeight + 1);
+                animation = "walkEast";
+
+                if (!IsCollision(tx, ty))
+                    _positionPerso.X += walkSpeed;
+                _perso.Play("walkEast");
+
+            }
+            //-=-=-=-=-=-=-=-=-=-GAUCHE-=-=-=-=-=-=-=-=-=-\\
+
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth - 1);
+                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileHeight + 1);
+                animation = "walkEast";
+
+                if (!IsCollision(tx, ty))
+                    _positionPerso.X -= walkSpeed;
+                _perso.Play("idle");
+            }
+            //-=-=-=-=-=-=-=-=-=-HAUT-=-=-=-=-=-=-=-=-=-\\
+
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth);
+                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileHeight);
+                animation = "walkNorth";
+
+                if (_keyboardState.IsKeyDown(Keys.Left))
+                    _perso.Play("walkEast");
+                if (_keyboardState.IsKeyDown(Keys.Right))
+                    _perso.Play("walkWest");
+                else
+                    _perso.Play("walkNorth");
+
+
+                if (!IsCollision(tx, ty))
+                    _positionPerso.Y -= walkSpeed;
+            }
+            //-=-=-=-=-=-=-=-=-=-BAS-=-=-=-=-=-=-=-=-=-\\
+
+            if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth);
+                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileHeight + 1.5);
+                animation = "walkSouth";
+
+                if (_keyboardState.IsKeyDown(Keys.Left))
+                    _perso.Play("walkEast");
+                if (_keyboardState.IsKeyDown(Keys.Right))
+                    _perso.Play("walkWest");
+                else
+                    _perso.Play("walkSouth");
+
+                if (!IsCollision(tx, ty))
+                    _positionPerso.Y += walkSpeed;
+            }
+
             base.Update(gameTime);
         }
-
+        private bool IsCollision(ushort x, ushort y)
+        {
+            // définition de tile qui peut être null (?)
+            TiledMapTile? tile;
+            if (mapLayer.TryGetTile(x, y, out tile) == false)
+                return false;
+            if (!tile.Value.IsBlank)
+                return true;
+            return false;
+        }
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Blue);
             _tiledMapRenderer.Draw();
-
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_perso, _positionPerso);
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
