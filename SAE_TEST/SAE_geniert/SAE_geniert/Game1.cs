692346bd12fda8c022e1c30e73fa2dcf59f35294
Valentin Falquet <Valentin.Falquet@etu.univ-smb.fr>
2023-01-05 09:35:06 +0100

    testmapfond

--- SAE_TEST/SAE_geniert/SAE_geniert/Game1.cs
+++ SAE_TEST/SAE_geniert/SAE_geniert/Game1.cs
@@ -94,7 +94,7 @@ namespace SAE_geniert
 
 
 
-                if (!IsCollision(tx, ty)) 
+                /*if (!IsCollision(tx, ty)) */
                     _positionPerso.X += walkSpeed;
                 _perso.Play("walkEast");
 
@@ -157,7 +157,7 @@ namespace SAE_geniert
 
             base.Update(gameTime);
         }
-        private bool IsCollision(ushort x, ushort y)
+        /*private bool IsCollision(ushort x, ushort y)
         {
             // définition de tile qui peut être null (?)
             TiledMapTile? tile;
@@ -166,14 +166,16 @@ namespace SAE_geniert
             if (!tile.Value.IsBlank)
                 return true;
             return false;
-        }
+        }*/
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Blue);
             _tiledMapRenderer.Draw();
+            _tiledMapRenderer.Draw();
             _spriteBatch.Begin();
             _spriteBatch.Draw(_perso, _positionPerso);
             _spriteBatch.End();
+            
             base.Draw(gameTime);
         }
     }
