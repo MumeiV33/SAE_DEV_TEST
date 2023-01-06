ee286fc12271063a50fd91bab3077c977335bc80
Valentin Falquet <Valentin.Falquet@etu.univ-smb.fr>
2023-01-06 17:56:25 +0100

    fgrtg

--- SAE_TEST/SAE_geniert/SAE_geniert/Game1.cs
+++ SAE_TEST/SAE_geniert/SAE_geniert/Game1.cs
@@ -24,13 +24,6 @@ namespace SAE_geniert
         private TiledMapRenderer _tiledMapRenderer;
         private TiledMapTileLayer mapLayer;
         private TiledMapTileLayer mapLayerTest;
-        
-
-        //-----> Perso
-        private Vector2 _positionPerso;
-        private AnimatedSprite _perso;
-        private int _sensPerso;
-        private int _vitessePerso;
 
         //-----> Tortue   /*]=-• COPY CODE TORTUE*/
         private Vector2 _positionTortue;
@@ -124,7 +117,7 @@ namespace SAE_geniert
             // TODO: Add your initialization logic here
             Window.Title = "Silver World";
             GraphicsDevice.BlendState = BlendState.AlphaBlend;
-            _positionPerso = new Vector2(300, 340);
+            Joueur._positionPerso = new Vector2(300, 340);
             _vitessePerso = 100;
             _positionTortue = new Vector2(300, 300);   /*]=-• COPY CODE TORTUE*/
             _vitesseTortue = 100;   /*]=-• COPY CODE TORTUE*/
@@ -155,93 +148,7 @@ namespace SAE_geniert
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-            _tiledMapRenderer.Update(gameTime);
-            base.Update(gameTime);
-
-            // TODO: Add your update logic here
-
-            //-----------------Déplacements-------------------------------------------------------------------
-            float deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds; // DeltaTime
-            float walkSpeed = deltaSeconds * _vitessePerso; // Vitesse de déplacement du sprite
-            KeyboardState keyboardState = Keyboard.GetState();
-            _perso.Update(deltaSeconds); // time écoulé
-
-            _keyboardState = Keyboard.GetState();
-            //-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
-
-            //-=-=-=-=-=-=-=-=-=-DROITE-=-=-=-=-=-=-=-=-=-\\
-
-            //-----------------Déplacements-------------------------------------------------------------------
-            if (keyboardState.IsKeyDown(Keys.Right))
-            {
-                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth + 0.5);
-                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileHeight + 0.8);
-            //-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
-
-
-
-                if (!IsCollision(tx, ty)) 
-                    _positionPerso.X += walkSpeed;
-                _perso.Play("walkEast");
-
-
-            }
-            //-=-=-=-=-=-=-=-=-=-GAUCHE-=-=-=-=-=-=-=-=-=-\\
-
-            //-----------------Déplacements-------------------------------------------------------------------
-            if (keyboardState.IsKeyDown(Keys.Left))
-            {
-                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth - 0.5);
-                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileHeight + 0.5);
-            //-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
-
-                if (!IsCollision(tx, ty))
-                    _positionPerso.X -= walkSpeed;
-                _perso.Play("walkWest");
-            }
-            //-=-=-=-=-=-=-=-=-=-HAUT-=-=-=-=-=-=-=-=-=-\\
-
-            //-----------------Déplacements-------------------------------------------------------------------
-            if (keyboardState.IsKeyDown(Keys.Up))
-            {
-                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth);
-                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileHeight);
             
-                if (_keyboardState.IsKeyDown(Keys.Left))
-                    _perso.Play("walkWest");
-                else if (_keyboardState.IsKeyDown(Keys.Right))
-                    _perso.Play("walkEast");
-                else
-                    _perso.Play("walkNorth");
-            //-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
-
-
-
-                if (!IsCollision(tx, ty))
-                    _positionPerso.Y -= walkSpeed;
-
-            }
-            //-=-=-=-=-=-=-=-=-=-BAS-=-=-=-=-=-=-=-=-=-\\
-
-            //-----------------Déplacements-------------------------------------------------------------------
-
-            if (keyboardState.IsKeyDown(Keys.Down))
-            {
-                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth);
-                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileHeight + 1);
-
-                if (_keyboardState.IsKeyDown(Keys.Left))
-                    _perso.Play("walkWest");
-                else if (_keyboardState.IsKeyDown(Keys.Right))
-                    _perso.Play("walkEast");
-                else
-                    _perso.Play("walkSouth");
-            //-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^
-                if (!IsCollision(tx, ty))
-                    _positionPerso.Y += walkSpeed;
-            }
             
             /*]=-• COPY CODE TORTUE*/
             //--------------Déplacements-Torute-----------------------------------------------------------
