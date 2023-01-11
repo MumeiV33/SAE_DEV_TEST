c18d02d84508efb393a2f0f713b8520c6cecee96
Alexandre DESCHAMPS <alexandre.deschamps@etu.univ-smb.fr>
2023-01-11 15:24:24 +0100

    jsp pk mis askip c bien

--- SAE_TEST/SAE_geniert/SAE_geniert/SceneGrotte.cs
+++ SAE_TEST/SAE_geniert/SAE_geniert/SceneGrotte.cs
@@ -158,69 +158,7 @@ namespace SAE_geniert
             _perso.Update(deltaSeconds); // time écoulé
             _keyboardState = Keyboard.GetState();
 
-            //-=-=-=-=-=-=-=-=-=-DROITE-=-=-=-=-=-=-=-=-=-\\
-                //-----------------Déplacements---------
-            if (keyboardState.IsKeyDown(Keys.Right))
-            {
-                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth + 0.5);
-                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileHeight + 0.8);
-                //-^-^-^-^-^-^-^-^--COLLISIONS--^-^-^-^-^-^-^-^
-                if (!IsCollision(tx, ty, _mapLayer))
-                    _positionPerso.X += walkSpeed;
-                _perso.Play("walkEast");
-            }
-
-            //-=-=-=-=-=-=-=-=-=-GAUCHE-=-=-=-=-=-=-=-=-=-\\
-                //-----------------Déplacements-------------
-            if (keyboardState.IsKeyDown(Keys.Left))
-            {
-                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth - 0.5);
-                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileHeight + 0.5);
-                //-^-^-^-^-^-^-^-^--COLLISIONS--^-^-^-^-^-^-^-^
-
-                if (!IsCollision(tx, ty, _mapLayer))
-                    _positionPerso.X -= walkSpeed;
-                _perso.Play("walkWest");
-            }
-
-            //-=-=-=-=-=-=-=-=-=-HAUT-=-=-=-=-=-=-=-=-=-\\
-                //-----------------Déplacements----------
-            if (keyboardState.IsKeyDown(Keys.Up))
-            {
-                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth);
-                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileHeight);
-
-                if (_keyboardState.IsKeyDown(Keys.Left))
-                    _perso.Play("walkWest");
-                else if (_keyboardState.IsKeyDown(Keys.Right))
-                    _perso.Play("walkEast");
-                else
-                    _perso.Play("walkNorth");
-                //-^-^-^-^-^-^-^-^--COLLISIONS--^-^-^-^-^-^-^-^
-
-                if (!IsCollision(tx, ty, _mapLayer))
-                    _positionPerso.Y -= walkSpeed;
-            }
-
-            //-=-=-=-=-=-=-=-=-=-BAS-=-=-=-=-=-=-=-=-=-\\
-                //-----------------Déplacements--------
-            if (keyboardState.IsKeyDown(Keys.Down))
-            {
-                ushort tx = (ushort)(_positionPerso.X / _tiledMap.TileWidth);
-                ushort ty = (ushort)(_positionPerso.Y / _tiledMap.TileHeight + 1);
-
-                if (_keyboardState.IsKeyDown(Keys.Left))
-                    _perso.Play("walkWest");
-                else if (_keyboardState.IsKeyDown(Keys.Right))
-                    _perso.Play("walkEast");
-                else
-                    _perso.Play("walkSouth");
-                //-^-^-^-^-^-^-^-^--COLLISIONS--^-^-^-^-^-^-^-^
-
-                if (!IsCollision(tx, ty, _mapLayer))
-                    _positionPerso.Y += walkSpeed;
-            }
-
+            
         }
 
 
